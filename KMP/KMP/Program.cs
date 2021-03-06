e477e497af1581b7869843f879a5ae561f80a41d
szx <810143012@qq.com>
2021-03-06 19:42:00 +0800

    提交工程

--- KMP/KMP/Program.cs
+++ KMP/KMP/Program.cs
@@ -5,5 +5,17 @@
         public static void Main(string[] args)
         {
         }
+
+
+        /// <summary>
+        /// 使用kmp算法查找子字符串的位置
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public static int KMPSearch(string str, string pattern)
+        {
+            return -1;
+        }
     }
 }
